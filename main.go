4ba70eda94b1ef98ee36de4237dc650aec82dc0e
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-10-18 21:30:30 -0500

    feat: Add handlers for each page
    Add handlers for the login, create, and read pages, as well as static
    content and a 404 handler.

--- main.go
+++ main.go
@@ -26,4 +26,20 @@ func initDB() {
 
 func main() {
 	initDB()
+
+	http.HandleFunc("/login", loginHandler)
+
+	http.HandleFunc("/create", createHandler)
+
+	http.HandleFunc("/read", readHandler)
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	http.HandleFunc("/", pageNotFoundHandler)
+
+	log.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatalf("Could not start server: %s\n", err.Error())
+	}
 }
